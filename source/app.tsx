28df17463719c190daeaf432e9c9cf105f824aae
Kaho Cheung <kaho@elefanttraks.com>
2024-03-13 21:43:53 +1100

    Cleanup

--- source/app.tsx
+++ source/app.tsx
@@ -8,42 +8,42 @@ import { PackageApp } from './package-app.js';
 import { AwLogo } from './aw-logo.js';
 
 type Props = {
-	state?: AppState;
-	gitAppState?: GitAppState;
+  state?: AppState;
+  gitAppState?: GitAppState;
 };
 
 export type AppState = 'test' | 'git' | 'package';
 
 const items = [
-	{ label: 'Git', value: 'git' },
-	{ label: 'Test', value: 'test' },
-	{ label: 'Package', value: 'package' },
+  { label: 'Git', value: 'git' },
+  { label: 'Test', value: 'test' },
+  // { label: 'Package', value: 'package' }, // WIP
 ];
 
 export default function App({ state, gitAppState }: Props) {
-	const [appState, setAppState] = useState<AppState>(state);
+  const [appState, setAppState] = useState<AppState>(state);
 
-	const handleSelect = (item: { label: string; value: AppState }) => {
-		setAppState(item.value);
-	};
+  const handleSelect = (item: { label: string; value: AppState }) => {
+    setAppState(item.value);
+  };
 
-	if (appState === 'git') {
-		return <GitApp state={gitAppState} />;
-	}
+  if (appState === 'git') {
+    return <GitApp state={gitAppState} />;
+  }
 
-	if (appState === 'test') {
-		return <TestApp />;
-	}
+  if (appState === 'test') {
+    return <TestApp />;
+  }
 
-	if (appState === 'package') {
-		return <PackageApp />;
-	}
+  if (appState === 'package') {
+    return <PackageApp />;
+  }
 
-	return (
-		<>
-			{/* <AwLogo /> */}
-			<Text color={'yellow'}>Choose an option:</Text>
-			<SelectInput items={items} onSelect={handleSelect} />
-		</>
-	);
+  return (
+    <>
+      {/* <AwLogo /> */}
+      <Text color={'yellow'}>Choose an option:</Text>
+      <SelectInput items={items} onSelect={handleSelect} />
+    </>
+  );
 }
